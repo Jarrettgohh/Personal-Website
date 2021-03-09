09b2c85ddd32cfd44bea6d0128eee6a2f5ef791d
Jarrettgohh <Jarrettgoh.xz@gmail.com>
2021-03-09 20:57:57 +0800

    basic layout 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,69 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
 import "./css/main.css";
 
 function App() {
   return (
     <React.Fragment>
-      <div style={{ marginBottom: 80, padding: 20 }}>
-        <img src="JS_logo.png" alt="Javascript Logo" style={{ height: 120 }} />
-        <img src="React_logo.png" alt="ReactJs Logo" style={{ height: 120 }} />
-        <img src="Node_logo.png" alt="NodeJs Logo" style={{ height: 120 }} />
-        <img src="Python_logo.png" alt="Python Logo" style={{ height: 120 }} />
-      </div>
+      <Container fluid>
+        <Row
+        // style={{
+        //   marginBottom: 80,
+        //   padding: 20,
+        // }}
+        >
+          <Col style={{ height: 100 }}>
+            <img
+              src="JS_logo.png"
+              alt="Javascript Logo"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+                overflow: "hidden",
+              }}
+            />
+          </Col>
+          <Col style={{ height: 100 }}>
+            <img
+              src="React_logo.png"
+              alt="ReactJs Logo"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+                overflow: "hidden",
+              }}
+            />
+          </Col>
+          <Col style={{ height: 100 }}>
+            <img
+              src="Node_logo.png"
+              alt="NodeJs Logo"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+                overflow: "hidden",
+              }}
+            />
+          </Col>
+          <Col style={{ height: 100 }}>
+            <img
+              src="Python_logo.png"
+              alt="Python Logo"
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "contain",
+                overflow: "hidden",
+              }}
+            />
+          </Col>
+        </Row>
+      </Container>
+
       <div style={{ padding: 20 }}>
         <h1>About myself</h1>
         <p className="text-style">
@@ -30,6 +84,7 @@ function App() {
           Automated programming together!
         </p>
       </div>
+
       <div style={{ padding: 20 }}>
         <h1>Citations for images used</h1>
         <p style={{ fontSize: 18 }}>
